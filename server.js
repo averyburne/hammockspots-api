448cfb1dca35daf3667af2d493354d4d14fe5110
Avery Burne <burneav@gmail.com>
2020-07-17 16:57:48 -0400

    removed example routes references in server.js

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 // require route files
-const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
 const hammockSpotRoutes = require('./app/routes/hammockSpot_routes')
-
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -63,7 +61,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(requestLogger)
 
 // register route files
-app.use(exampleRoutes)
 app.use(userRoutes)
 app.use(hammockSpotRoutes)
 
